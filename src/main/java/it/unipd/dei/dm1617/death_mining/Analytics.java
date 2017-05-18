e8a118703569250f521c41ecf00c4cfd9fe37a17
tonca <istsamtonca@gmail.com>
2017-05-18 18:44:27 +0200

    Reverted bad 'optimization' :/

--- src/main/java/it/unipd/dei/dm1617/death_mining/Analytics.java
+++ src/main/java/it/unipd/dei/dm1617/death_mining/Analytics.java
@@ -1,10 +1,13 @@
 package it.unipd.dei.dm1617.death_mining;
 
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import com.clearspring.analytics.util.Lists;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
+import org.apache.spark.mllib.fpm.FPGrowth;
+import org.apache.spark.mllib.fpm.FPGrowthModel;
+import scala.Array;
 import scala.Tuple2;
 
 import java.io.File;
@@ -14,13 +17,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 /**
  * Created by tonca on 11/05/17.
  *
  * This is a class for generic statistical analysis of the dataset
+ *
+ *
  *
  */
 public class Analytics {
@@ -61,38 +64,29 @@ public class Analytics {
         long transactionsCount = transactions.count();
         Broadcast<Long> bCount = sc.broadcast(transactionsCount);
 
-        List<Tuple2<String,List<String>>> outputs = transactions
-            .map((itemset) -> itemset.stream())
-            .mapPartitionsToPair((itemset) -> {
-                // This map holds the counts for each word in each partition.
-                Object2IntOpenHashMap<Property> partitionCounts = new Object2IntOpenHashMap<>();
-                partitionCounts.defaultReturnValue(0);
-                while (itemset.hasNext()) {
-                    Property item = itemset.next().findAny().get();
-                    int oldCnt = partitionCounts.getOrDefault(item, 0);
-                    partitionCounts.put(item, oldCnt + 1);
-                }
-                return partitionCounts
-                        .entrySet().stream()
-                        .map((entry) -> new Tuple2<>(entry.getKey(), entry.getValue()))
-                        .iterator();
-            })
-            .reduceByKey((x, y) -> x + y)
-            .groupBy(item -> item._1()._1())
-            .map((item) -> new Tuple2<>(item._1(), StreamSupport
-                    .stream(item._2().spliterator(), false)
-                    .map((elem) -> elem._1() + ", " + ((float)elem._2()/bCount.value()))
-                    .collect(Collectors.toList())))
-            .collect();
+        List<Tuple2<String,ArrayList<String>>> outputs = transactions
+                .flatMap(itemset -> itemset.iterator())
+                .mapToPair(item -> new Tuple2<>(item, 1))
+                .reduceByKey((a, b) -> a + b)
+                .groupBy(item -> item._1()._1())
+                .map((item) -> {
+                    ArrayList<String> lines = new ArrayList<>();
+                    for( Tuple2<Property,Integer> counted : item._2()) {
+                        String line = counted._1() + ", " + ((float)counted._2()/bCount.value());
+                        lines.add(line);
+                    }
+                    return new Tuple2<>(item._1(),lines);
+                })
+                .collect();
 
 
         File directory = new File("results/stats/");
         if (! directory.exists()){
             directory.mkdirs();
         }
-        for(Tuple2<String,List<String>> colList : outputs ) {
+        for(Tuple2<String,ArrayList<String>> colList : outputs ) {
 
-            Path file = Paths.get("results/stats/"+fd.value().decodeColumn(colList._1())+"_stats.txt");
+            Path file = Paths.get("results/stats/"+colList._1()+"_stats.txt");
             try {
                 Files.write(file, colList._2(), Charset.forName("UTF-8"));
             }
