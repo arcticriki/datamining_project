357692bb628fd56300de45b4f754338a61a2202d
Gianluca Marcon <marcon.gluca@gmail.com>
2017-05-27 23:44:57 +0200

    Created DeathSaver class, containing two methods to easily save itemsets and rules in a CSV file WITH COLUMN NAMES.

---
+++ src/main/java/it/unipd/dei/dm1617/death_mining/DeathSaver.java
@@ -0,0 +1,65 @@
+package it.unipd.dei.dm1617.death_mining;
+
+import org.apache.hadoop.yarn.webapp.hamlet.Hamlet;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import scala.Tuple2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gianluca on 27/05/17.
+ */
+public class DeathSaver {
+    public static void saveItemsets(String path, JavaPairRDD<List<Property>, Double> freqItemsets){
+
+
+        try {
+            Path outputFile = Paths.get(path);
+            if (!Files.exists(outputFile.getParent()))
+                Files.createDirectory(outputFile.getParent());
+
+            List<String> lines = new ArrayList<>();
+            lines.add("itemset;support");
+            lines.addAll(
+                freqItemsets
+                    .mapToPair(Tuple2::swap)
+                    .sortByKey(false)
+                    .map(i -> i._2.toString() + ";" + i._1.toString())
+                    .collect()
+            );
+
+            Files.write(outputFile, lines, Charset.forName("UTF-8"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void saveRules(String path, JavaRDD<ExtendedRule> rules){
+        try {
+            Path outputFile = Paths.get(path);
+            if (!Files.exists(outputFile.getParent()))
+                Files.createDirectory(outputFile.getParent());
+
+            List<String> lines = new ArrayList<>();
+            lines.add("rule;support;confidence;lift;conviction");
+            lines.addAll(
+                    rules.sortBy(ExtendedRule::getConfidence, false, 1)
+                            .map(ExtendedRule::CSVformat)
+                            .collect()
+            );
+
+            Files.write(outputFile, lines, Charset.forName("UTF-8"));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
