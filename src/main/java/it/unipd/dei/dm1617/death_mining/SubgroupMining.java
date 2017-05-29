9839a04899d32a9493ec6053e3cd376decc8bfe9
tonca <istsamtonca@gmail.com>
2017-05-29 18:14:23 +0200

    Removing too frequent items dynamically

--- src/main/java/it/unipd/dei/dm1617/death_mining/SubgroupMining.java
+++ src/main/java/it/unipd/dei/dm1617/death_mining/SubgroupMining.java
@@ -4,8 +4,8 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
-
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -30,22 +30,46 @@ public class SubgroupMining {
 
         // Removing too frequent items and selecting subgroup
         transactions = transactions.filter( itemset -> {
-            boolean contains = false;
+                boolean contains = false;
+                for (Property item : itemset) {
+                    if (item._1().equals(colSelection) && item._2().equals(valSelection))
+                        contains = true;
+                }
+                return Boolean.valueOf(contains);
+            })
+            .map(transaction -> {
+                Property reject = null;
+                for (Property item : transaction) {
+                    if (item._1().equals(colSelection))
+                        reject = item;
+                }
+                transaction.remove(reject);
+                return transaction;
+            });
+
+        long transactionsCount = transactions.count();
+        Broadcast<Long> bCount = sc.broadcast(transactionsCount);
+
+        // FILTERING TOO FREQUENT ITEMS
+        List<Property> outputs = transactions
+                .flatMap(itemset -> itemset.iterator())
+                .mapToPair(item -> new Tuple2<>(item, 1))
+                .reduceByKey((a, b) -> a + b)
+                .filter(item -> Boolean.valueOf((1.0*item._2())/bCount.getValue()>0.6))
+                .map(item -> item._1())
+                .collect();
+
+        System.out.println("Too frequent items: "+outputs);
+
+        transactions = transactions.map(itemset -> {
+            List<Property> tmp = new ArrayList<>();
             for (Property item : itemset) {
-                if (item._1().equals(colSelection) && item._2().equals(valSelection))
-                    contains = true;
+                if (!outputs.contains(item)) {
+                    tmp.add(item);
+                }
             }
-            return Boolean.valueOf(contains);
-        })
-                .map(transaction -> {
-                    Property reject = null;
-                    for (Property item : transaction) {
-                        if (item._1().equals(colSelection))
-                            reject = item;
-                    }
-                    transaction.remove(reject);
-                    return transaction;
-                });
+            return tmp;
+        });
 
         // mine frequent itemsets and association rules
         DeathMiner dm = new DeathMiner(sc, transactions);
@@ -90,10 +114,13 @@ public class SubgroupMining {
         subgroups.put("Sex", Arrays.asList("M", "F"));
         subgroups.put("Binned Age", Arrays.asList("Baby", "Child", "Teenager", "Adult", "Old"));
         subgroups.put("RaceRecode3", Arrays.asList("White", "Black", "Races other than White or Black"));
+        subgroups.put("Death Category", Arrays.asList("Homicide", "Suicide", "Accident"));
+
 
         for(Map.Entry<String, List<String>> entry : subgroups.entrySet())
         {
             for(String val : entry.getValue()) {
+                System.out.println(String.format("[subgroup selection] %s : %s",entry.getKey(),val));
                 singleGroupMining(sc, transactions, entry.getKey(), val, minSup);
             }
         }
